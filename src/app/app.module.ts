649bcd537c8f2bc081a985a6ecf37d2c9643dabc
Aphelele_Ndlela <aphelelendlela95@gmail.com>
2023-09-15 15:50:20 +0200

    Api connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common'
 import {MatCardModule} from '@angular/material/card';
-import { CommonModule } from '@angular/common';
-// import { Course } from './course';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -29,7 +27,7 @@ import { CourseDetailComponent } from './course-detail/course-detail.component';
     AddCourseComponent,
     DashboardComponent,
     CoursesComponent,
-    // CourseDetailComponent
+    CourseDetailComponent
   ],
   imports: [
     BrowserModule,
@@ -45,7 +43,7 @@ import { CourseDetailComponent } from './course-detail/course-detail.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    CommonModule
+    CommonModule,MatCardModule
   ],
 
   providers: [],
